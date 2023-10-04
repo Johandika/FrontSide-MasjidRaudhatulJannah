fb92d9a25f2f1560458f75faab4d8bf4cdec210c
Johandika <55685350+Johandika@users.noreply.github.com>
2023-10-04 08:46:28 +0700

    Paarsing Kantor

--- src/pages/TahsinAnak.jsx
+++ src/pages/TahsinAnak.jsx
@@ -1,7 +1,180 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "../style";
+
+const tahsins = [
+  {
+    id: 1,
+    kategori: "Tahsin Ikhwan",
+    hari: [
+      {
+        namaHari: "Senin dan Rabu",
+        kelas: [
+          {
+            namaKelas: "1A",
+            pemateri: "Ust. Yuslan",
+            peserta: ["Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "2A",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "2B",
+            pemateri: "Ust. Yuslan",
+
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "3A",
+            pemateri: "Ust. Anum",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+        ],
+      },
+      {
+        namaHari: "Selasa dan Kamis",
+        kelas: [
+          {
+            namaKelas: "1A",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "1B",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "2A",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "2B",
+            pemateri: "Ust. Yuslan",
+
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "3A",
+            pemateri: "Ust. Anum",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+        ],
+      },
+      {
+        namaHari: "Jumat dan Ahad",
+        kelas: [
+          {
+            namaKelas: "1A",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "1B",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "2A",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "2B",
+            pemateri: "Ust. Yuslan",
+
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "3A",
+            pemateri: "Ust. Anum",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+        ],
+      },
+    ],
+  },
+];
 
 const TahsinAnak = () => {
-  return <div>TahsinAnak</div>;
+  const [categoryActive, setCategoryActive] = useState("Tahsin Ikhwan");
+  const [hariActive, setHariActive] = useState("Senin dan Rabu");
+
+  // Filter pertama untuk kategori
+  const filteredTahsins = tahsins.filter(
+    (tahsin) => categoryActive === tahsin.kategori
+  );
+
+  // Filter kedua untuk hari
+  const filteredKelas = tahsins.flatMap((tahsin) =>
+    tahsin.hari
+      .filter((hari) => hari.namaHari === hariActive)
+      .flatMap((hari) => hari.kelas)
+  );
+
+  // Menggabungkan hasil kedua filter
+  const combinedFilter = [...filteredTahsins, ...filteredKelas];
+
+  return (
+    <div
+      className={`${styles.paddingX} ${styles.flexCenter} pt-[60px] bg-Gray1`}
+    >
+      <div className={`${styles.boxWidth}  my-20 flex flex-col gap-20`}>
+        <div>
+          <h1>Filter Data Tahsins</h1>
+          <div>
+            <label>Kategori Aktif:</label>
+            <select
+              value={categoryActive}
+              onChange={(e) => setCategoryActive(e.target.value)}
+            >
+              {/* Tambahkan opsi kategori sesuai data yang ada */}
+              {tahsins.map((tahsin) => (
+                <option
+                  key={tahsin.id}
+                  value={tahsin.kategori}
+                >
+                  {tahsin.kategori}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <label>Hari Aktif:</label>
+            <select
+              value={hariActive}
+              onChange={(e) => setHariActive(e.target.value)}
+            >
+              {/* Tambahkan opsi hari sesuai data yang ada */}
+              {tahsins
+                .flatMap((tahsin) => tahsin.hari)
+                .map((hari) => (
+                  <option
+                    key={hari.namaHari}
+                    value={hari.namaHari}
+                  >
+                    {hari.namaHari}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div>
+            <h2>Hasil Gabungan Filter:</h2>
+            <ul>
+              {combinedFilter.map((item) => (
+                <li key={item.namaKelas || item.kategori}>
+                  {/* Tampilkan data sesuai dengan kondisi */}
+                  {item.namaKelas || item.kategori}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TahsinAnak;
--- src/pages/TahsinIkhwan.jsx
+++ src/pages/TahsinIkhwan.jsx
@@ -1,23 +1,219 @@
+import { useState } from "react";
 import styles from "../style";
 
+const tahsins = [
+  {
+    id: 1,
+    kategori: "Tahsin Ikhwan",
+    hari: [
+      {
+        namaHari: "Senin dan Rabu",
+        kelas: [
+          {
+            namaKelas: "1A",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "1B",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "2A",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "2B",
+            pemateri: "Ust. Yuslan",
+
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "3A",
+            pemateri: "Ust. Anum",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+        ],
+      },
+      {
+        namaHari: "Selasa dan Kamis",
+        kelas: [
+          {
+            namaKelas: "1A",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "1B",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "2A",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "2B",
+            pemateri: "Ust. Yuslan",
+
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "3A",
+            pemateri: "Ust. Anum",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+        ],
+      },
+      {
+        namaHari: "Jumat dan Ahad",
+        kelas: [
+          {
+            namaKelas: "1A",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "1B",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "2A",
+            pemateri: "Ust. Yuslan",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "2B",
+            pemateri: "Ust. Yuslan",
+
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+          {
+            namaKelas: "3A",
+            pemateri: "Ust. Anum",
+            peserta: ["johandika", "Naufal", "Hanif", "Abdul"],
+          },
+        ],
+      },
+    ],
+  },
+];
+
 const TahsinIkhwan = () => {
+  const [categoryActive, setCategoryActive] = useState("Tahsin Ikhwan");
+  const [hariActive, setHariActive] = useState("Senin dan Rabu");
+
+  // Filter pertama untuk kategori
+  const filteredTahsins = tahsins.filter(
+    (tahsin) => categoryActive === tahsin.kategori
+  );
+
+  // Filter kedua untuk hari
+  const filteredKelas = tahsins.flatMap((tahsin) =>
+    tahsin.hari
+      .filter((hari) => hari.namaHari === hariActive)
+      .flatMap((hari) => hari.kelas)
+  );
+
+  // Menggabungkan hasil kedua filter
+  const combinedFilter = [...filteredTahsins, ...filteredKelas];
+
   return (
-    <div className={`${styles.paddingX} ${styles.flexCenter} pt-[60px] bg-Gray1`}>
+    <div
+      className={`${styles.paddingX} ${styles.flexCenter} pt-[60px] bg-Gray1`}
+    >
       <div className={`${styles.boxWidth}  my-20 flex flex-col gap-20`}>
+        <div className="flex flex-wrap flex-row gap-4">
+          {/* Badge */}
+          {tahsins.map((tahsin) => (
+            <div
+              className={`px-4 py-2 bg-white rounded-md shadow-sm cursor-pointer  ${
+                categoryActive === tahsin.kategori
+                  ? "bg-greenText text-white font-bold"
+                  : "hover:bg-yellow hover:scale-105 transition"
+              }`}
+              key={tahsin.kategori}
+              onClick={() => {
+                setCategoryActive(tahsin.kategori);
+              }}
+            >
+              {tahsin.kategori}
+            </div>
+          ))}
+
+          {tahsins.map((tahsin) =>
+            tahsin.hari.map((hari) => (
+              <div
+                className={`px-4 py-2 bg-white rounded-md shadow-sm cursor-pointer  ${
+                  hariActive === hari.namaHari
+                    ? "bg-greenText text-white font-bold"
+                    : "hover:bg-yellow hover:scale-105 transition"
+                }`}
+                key={hari.namaHari}
+                onClick={() => {
+                  setHariActive(hari.namaHari);
+                }}
+              >
+                {hari.namaHari}
+              </div>
+            ))
+          )}
+        </div>
         {/* Judul dan Deskripsi */}
         <div className="flex flex-col justify-center items-center text-center gap-9">
           <h1 className="text-judul ">Jadwal Tahsin Ikhwan</h1>
           <p className="w-full sm:w-[90%] md:w-[70%]">
-            Tahsin ikhwan adalah program tahsin yang dimiliki oleh RJIC untuk peserta tahsin
-            laki-laki dewasa.
+            Tahsin ikhwan adalah program tahsin yang dimiliki oleh RJIC untuk
+            peserta tahsin laki-laki dewasa.
           </p>
         </div>
-        <div className="grid grid-cols-4 ">
-          <div className="bg-white shadow rounded-md p-5">asdf</div>
-          <div>adsfa</div>
-          <div>asdfa</div>
-          <div>asdf</div>
-        </div>
+        {/* Card */}
+        {combinedFilter.map((item) => (
+          <li key={item.namaKelas || item.kategori}>
+            <div
+              className="grid grid-cols-4 gap-4"
+              key={item.id}
+            >
+              {/* Perulangan */}
+              <div
+                className="flex flex-1 justify-start flex-col  bg-white shadow rounded-md p-5 gap-4"
+                key={item.namaKelas}
+              >
+                <div className="text-center">
+                  <div className="text-greenText font-bold text-2xl">
+                    {item.namaKelas || item.kategori}
+                  </div>
+                </div>
+                <hr />
+                <div className="text-greenBackground font-bold">
+                  {item.pemateri}
+                </div>
+                <div className="flex flex-col gap-1">
+                  <p className="text-neutral-500 font-base text-sm">
+                    Peserta :
+                  </p>
+                  <ol className="text-neutral-500 font-semibold">
+                    {item.namaKelas ? (
+                      // Ini adalah kelas, maka tampilkan peserta
+                      item.peserta.map((peserta, index) => (
+                        <li key={index}>
+                          {index + 1}. {peserta}
+                        </li>
+                      ))
+                    ) : (
+                      // Ini adalah kategori, maka tidak perlu menampilkan peserta
+                      <li>Tidak ada peserta</li>
+                    )}
+                  </ol>
+                </div>
+              </div>
+            </div>
+          </li>
+        ))}
       </div>
     </div>
   );
