4445d5d475f0cbb100b3f5d2e6a14850fa806564
Johandika <55685350+Johandika@users.noreply.github.com>
2023-10-10 15:55:57 +0700

    bug fixing navbar

--- src/components/layout/NavbarHome.jsx
+++ src/components/layout/NavbarHome.jsx
@@ -1,14 +1,16 @@
 import React, { useCallback, useEffect, useState } from "react";
 
 const NavbarHome = ({ children }) => {
-  const [navbarBg, setNavbarBg] = useState("bg-transparent text-white");
-  const [active, setActive] = useState("Home");
+  const initialActive = localStorage.getItem("active") || "Home";
 
-  const dataUntukChildren = {
-    active,
-    setActive,
-  };
+  const [active, setActive] = useState(initialActive);
+  const [navbarBg, setNavbarBg] = useState(
+    initialActive === "Home"
+      ? "bg-transparent text-white"
+      : "bg-white border-b-[1px] text-black"
+  );
 
+  // Function untuk handle style saat scroll
   const handleScroll = useCallback(() => {
     if (window.scrollY === 0 && active === "Home") {
       setNavbarBg("bg-transparent text-white ");
@@ -18,21 +20,32 @@ const NavbarHome = ({ children }) => {
   }, [active]);
 
   useEffect(() => {
-    // handleScroll jalan saat scroll
     window.addEventListener("scroll", handleScroll);
 
-    // Hapus event listener saat komponen di-unmount
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [active, handleScroll]);
+  }, [handleScroll]);
 
+  // Set local storage sesuai state active
+  useEffect(() => {
+    localStorage.setItem("active", active);
+
+    if (active === "Home") {
+      setNavbarBg("bg-transparent text-white ");
+    } else {
+      setNavbarBg("bg-white border-b-[1px] text-black");
+    }
+  }, [active]);
+
+  const dataUntukChildren = {
+    active,
+    setActive,
+  };
+
+  console.log(active);
   return (
-    <header
-      className={`transition duration-200 ${
-        active === "Home" ? navbarBg : "text-black bg-white border-b-[1px]"
-      } `}
-    >
+    <header className={`transition duration-200 ${navbarBg} `}>
       {React.cloneElement(children, { ...dataUntukChildren })}
     </header>
   );
